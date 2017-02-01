2410e37e5ada7599320f8a3d64cb6c7786135142
Michiel Tombergen <michielhttombergen@gmail.com>
2017-02-01 15:48:41 +0100

    Controller klasse is aangevuld en werkend gehouden.

--- ParkingSimulatorBV/src/controller/Controller.java
+++ ParkingSimulatorBV/src/controller/Controller.java
@@ -6,11 +6,17 @@ import java.awt.event.*;
 import view.*;
 
 public class Controller extends AbstractController implements ActionListener {
+	
 	private static final long serialVersionUID = -7413164724294460746L;
+	
 	private JButton mineen;
 	private JButton pluseen;
 	private JButton start;
 	private JButton stop;
+	
+	public static final String AD_HOC = "1";
+	public static final String PASS = "2";
+	
 	private CarQueue entranceCarQueue;
     private CarQueue entrancePassQueue;
     private CarQueue paymentCarQueue;
@@ -83,4 +89,35 @@ public class Controller extends AbstractController implements ActionListener {
     	simulatorView.updateView();
 }
 	
+    /**
+     * Verwijder een auto van zijn locatie en laat hem in de rij van de uitgang staan. 
+     * @param car
+     */
+    public void carLeavesSpot(Car car){
+    	simulatorView.removeCarAt(car.getLocation());
+        exitCarQueue.addCar(car);
+    }
+   
+    /**
+     * Voeg aankomende auto's toe.
+     * @param numberOfCars
+     * @param type
+     */
+    public void addArrivingCars(int numberOfCars, String type){
+        // Add the cars to the back of the queue.
+    	switch(type) {
+    	case AD_HOC: 
+            for (int i = 0; i < numberOfCars; i++) {
+            	entranceCarQueue.addCar(new AdHocCar());
+            }
+            break;
+    	case PASS:
+            for (int i = 0; i < numberOfCars; i++) {
+            	entrancePassQueue.addCar(new ParkingPassCar());
+            }
+            break;	            
+    	}
+    
+    }
+    
 }
